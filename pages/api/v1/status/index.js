bd64ada982473fd6d26496557291c7cade1122a5
Ismael Lunkes <lunkes86@gmail.com>
2023-10-22 00:32:06 +0000

    Configurado comunicação da aplicação com a base via docker

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,9 @@
-function status(request, response) {
-response.status(200).json({"chave" : "verificar UTF-8 é ã"});
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("select 1 + 1 AS SUM");
+  console.log(result.rows);
+  response.status(200).json({ chave: "testando UTF-8 é ã" });
 }
 
 export default status;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Cabecalho from "./cabecalho";
 
 function Home() {
-  return <h1>TESTE MUCHO LOCO Alterado.</h1>;
+  return <h1>ISMAEL - CODE IN THE VEIA</h1>;
 }
 
 function teste() {
